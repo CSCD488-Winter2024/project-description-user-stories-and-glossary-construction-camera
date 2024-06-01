24feaaddf8502aae9abe604653a7a60d4d4d4526
connerhanson <connerhanson0826@gmail.com>
2024-06-01 15:01:51 -0700

    all is working except delete user and change user credentaisl, api and endpoints are implemented, but are being buggy, still debugging

--- Webpages-Website/api.py
+++ Webpages-Website/api.py
@@ -1,9 +1,15 @@
 import flask
 import json
 import mariadb
+from flask import Flask
+from flask_cors import CORS
+from flask import request
+from flask import redirect
 
 
 app = flask.Flask(__name__)
+CORS(app)
+CORS(app, origins='*')
 app.config["DEBUG"] = True
 
 
@@ -20,7 +26,9 @@ config = {
 def index():
     conn = mariadb.connect(**config)
     cur = conn.cursor()
-    cur.execute("SELECT COUNT(*) FROM users WHERE username='johndoe' AND password='Password123'")
+    username = request.args.get('username')
+    password = request.args.get('password')
+    cur.execute("SELECT COUNT(*) FROM users WHERE username=%s AND password=%s", (username, password))
 
     row_header=[x[0] for x in cur.description]
     rv = cur.fetchall()
@@ -28,23 +36,110 @@ def index():
     
     for result in rv:
         json_data.append(dict(zip(row_header,result)))
-    
-    return json.dumps(json_data)
 
-@app.route('/api/adminStatus', methods=['GET'])
-def check_admin_status():
+    if json_data[0]['COUNT(*)'] > 0:
+        return redirect('http://localhost:8000/videoPage.html')
+    else:
+        return "User or Password Incorrect"
+
+
+
+
+@app.route('/api/changeUsername', methods=['GET'])
+def change_username():
     conn = mariadb.connect(**config)
     cur = conn.cursor()
-    cur.execute("SELECT admin_status FROM users WHERE username='johndoe'")
+    curUsername = request.args.get('curUsername')
+    newUsername = request.args.get('newUsername')
+    cur.execute("UPDATE users SET username=%s WHERE username=%", (newUsername, curUsername))
 
-    admin_status = cur.fetchone()
+@app.route('/api/changePassword', methods=['GET'])
+def change_password():
+    conn = mariadb.connect(**config)
+    cur = conn.cursor()
+    curPassword = request.args.get('curPassword')
+    newPassword = request.args.get('newPassword')
+    cur.execute("UPDATE users SET password=%s WHERE password=%s", (newPassword, curPassword))
 
-    if admin_status and admin_status[0] == 1:
-        return json.dumps({'admin_status': 1}), 200
+
+@app.route('/api/changeEmail', methods=['GET'])
+def change_email():
+    conn = mariadb.connect(**config)
+    cur = conn.cursor()
+    curEmail = request.args.get('curEmail')
+    newEmail = request.args.get('newEmail')
+    cur.execute("UPDATE users SET email=%s WHERE email=%s", (newEmail, curEmail))   
+
+@app.route('/api/changePhoneNumber', methods=['GET'])
+def change_phone():
+    conn = mariadb.connect(**config)
+    cur = conn.cursor()
+    curPhoneNumber = request.args.get('curPhoneNumber')
+    newPhoneNumber = request.args.get('newPhoneNumber')
+    cur.execute("UPDATE users SET phone_number=%s WHERE phone_number=%s", (newPhoneNumber, curPhoneNumber))
+   
+
+
+
+
+@app.route('/api/fetchUserData', methods=['GET'])
+def fetch_user_profile():
+    conn = mariadb.connect(**config)
+    cur = conn.cursor()
+    username = request.args.get('username')
+    cur.execute("SELECT name, email, phone_number FROM users WHERE username=%s", (username,))
+
+    profile_data = cur.fetchone()
+
+    if profile_data:
+        profile = {
+            'name': profile_data[0],
+            'email': profile_data[1],
+            'phone_number': profile_data[2]
+        }
+        return json.dumps(profile), 200
     else:
-        return json.dumps({'admin_status': 0}), 200
+        return "User not found", 404
 
-    
-    
+
+@app.route('/api/deleteUser', methods=["GET"])
+def delete_user():
+    conn = mariadb.connect(**config)
+    cur = conn.cursor()
+    data = request.json
+    email = data('email')
+    password = data('password')
+    username = request.args.get('username')
+    cur.execute("DELETE FROM users WHERE email=%s AND WHERE password=%s", (email, password))
+    conn.commit()
+    conn.close()
+    return jsonify({'success': True})
+
+
+
+@app.route('/api/updateUserData', methods=['POST'])
+def update_user_data():
+    conn = mariadb.connect(**config)
+    cur = conn.cursor()
+    field = data['field']
+    value = data['value']
+    username = request.args.get('username')
+
+    if field == 'username':
+        cur.execute("UPDATE users SET username=%s WHERE username=%s", (value, username))
+    elif field == 'password':
+        cur.execute("UPDATE users SET password=%s WHERE username=%s", (password, username))
+    elif field == 'email':
+        cur.execute("UPDATE users SET email=%s WHERE username=%s", (value, username))
+    elif field == 'phone_number':
+        cur.execute("UPDATE users SET phone_number=%s WHERE username=%s", (value, username))
+    else:
+        return jsonify({'success': False, 'message': 'Invalid field'})
+
+    conn.commit()
+    conn.close
+    return jsonify({'success': True})
+
+
 
 app.run()
